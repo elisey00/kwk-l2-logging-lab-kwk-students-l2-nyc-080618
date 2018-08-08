7397a616274ec1460d04f73e495a90c408f5baba
elisey00 <eliseyoung000@gmail.com>
2018-08-08 15:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const driver = 'bob';
 console.log("this code was called");
 console.log(driver);
-console.log(invoke())
+console.log(driver,"bob")
 
-function invoke(a1,a2){
-  var a1=var driver;
-  var a2= 
-  
-}
